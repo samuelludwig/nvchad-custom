28a776d1374bf6dcd697f0ed7eb1ee96029ff672
Samuel Ludwig <SLudwig.dev@gmail.com>
2023-01-20 09:50:00 -0500

    Run lsp-format on plugins/{init, null-ls}.lua

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,140 +1,138 @@
 local lisp_file_types = {
-  "clojure",
-  "clojurescript",
-  "edn",
-  "fennel",
-  "janet",
-  "lisp",
-  "hy",
+	"clojure",
+	"clojurescript",
+	"edn",
+	"fennel",
+	"janet",
+	"lisp",
+	"hy",
 }
 
 return {
 
-  -- Install plugin
-  ["Pocco81/TrueZen.nvim"] = {},
+	-- Install plugin
+	["Pocco81/TrueZen.nvim"] = {},
 
-  -- Override plugin definition options
-  ["goolord/alpha-nvim"] = {
-    disable = false,
-  },
+	-- Override plugin definition options
+	["goolord/alpha-nvim"] = {
+		disable = false,
+	},
 
-  ["folke/which-key.nvim"] = {
-    disable = false,
-  },
+	["folke/which-key.nvim"] = {
+		disable = false,
+	},
 
-  ["ojroques/vim-oscyank"] = {
-    branch = "main",
-  },
+	["ojroques/vim-oscyank"] = {
+		branch = "main",
+	},
 
-  -- LSP stuff
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end,
-  },
+	-- LSP stuff
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.lspconfig")
+		end,
+	},
 
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-      after = "nvim-lspconfig",
-      config = function()
-         require "custom.plugins.null-ls"
-      end,
-  },
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null-ls")
+		end,
+	},
 
-  -- TS needs certain c++ libraries present
-  ["nvim-treesitter/nvim-treesitter"] = {
-    override_options = {
-      ensure_installed = {
-        "lua",
-        "clojure",
-        "php",
-        "dockerfile",
-        "yaml",
-        "bash",
-        "typescript",
-        "javascript",
-        "html",
-        "css",
-        "python",
-        "vim",
-      },
-    },
-  },
+	-- TS needs certain c++ libraries present
+	["nvim-treesitter/nvim-treesitter"] = {
+		override_options = {
+			ensure_installed = {
+				"lua",
+				"clojure",
+				"php",
+				"dockerfile",
+				"yaml",
+				"bash",
+				"typescript",
+				"javascript",
+				"html",
+				"css",
+				"python",
+				"vim",
+			},
+		},
+	},
 
-  -- These will require npm, and go
-  ["williamboman/mason.nvim"] = {
-     override_options = {
-        ensure_installed = {
-          -- lua stuff
-          "lua-language-server",
-          "stylua",
+	-- These will require npm, and go
+	["williamboman/mason.nvim"] = {
+		override_options = {
+			ensure_installed = {
+				-- lua stuff
+				"lua-language-server",
+				"stylua",
 
-          -- web dev
-          "css-lsp",
-          "html-lsp",
-          "typescript-language-server",
-          "deno",
-          "emmet-ls",
-          "json-lsp",
-          "intelephense",
-          "eslint_d",
-          "prettierd",
+				-- web dev
+				"css-lsp",
+				"html-lsp",
+				"typescript-language-server",
+				"deno",
+				"emmet-ls",
+				"json-lsp",
+				"intelephense",
+				"eslint_d",
+				"prettierd",
 
-          -- python
-          "python-lsp-server",
-          "pyright",
+				-- python
+				"python-lsp-server",
+				"pyright",
 
-          -- infra/db
-          "nginx-language-server",
-          "sqls",
-          "sql-formatter",
-          "ansible-language-server",
-          "dockerfile-language-server",
-          "yaml-language-server",
+				-- infra/db
+				"nginx-language-server",
+				"sqls",
+				"sql-formatter",
+				"ansible-language-server",
+				"dockerfile-language-server",
+				"yaml-language-server",
 
-          -- shell
-          "shfmt",
-          "shellcheck",
+				-- shell
+				"shfmt",
+				"shellcheck",
 
-          --lisps
-          "clojure-lsp",
-        },
-      },
-    },
+				--lisps
+				"clojure-lsp",
+			},
+		},
+	},
 
-  ["tpope/vim-fugitive"] = {},
+	["tpope/vim-fugitive"] = {},
 
-  ["lewis6991/gitsigns.nvim"] = {
-    -- Use <leader>gb to get the blame line on-demand
-    -- override_options = {
-    --   current_line_blame = true,
-    -- },
-  },
+	["lewis6991/gitsigns.nvim"] = {
+		-- Use <leader>gb to get the blame line on-demand
+		-- override_options = {
+		--   current_line_blame = true,
+		-- },
+	},
 
-  -- Lisp stuff
-  ["Olical/conjure"] = {
-    ft = lisp_file_types,
-  },
+	-- Lisp stuff
+	["Olical/conjure"] = {
+		ft = lisp_file_types,
+	},
 
-  ["gpanders/nvim-parinfer"] = {
-    ft = lisp_file_types,
-  },
+	["gpanders/nvim-parinfer"] = {
+		ft = lisp_file_types,
+	},
 
-  ["clojure-vim/vim-jack-in"] = {
-    ft = {"clojure"},
----    requires = {"radenling/vim-dispatch-neovim"},
-  },
-  ["radenling/vim-dispatch-neovim"] = {},
-  ["tpope/vim-dispatch"] = {},
+	["clojure-vim/vim-jack-in"] = {
+		ft = { "clojure" },
+		---    requires = {"radenling/vim-dispatch-neovim"},
+	},
+	["radenling/vim-dispatch-neovim"] = {},
+	["tpope/vim-dispatch"] = {},
 
+	-- Motion plugins
+	["ggandor/leap.nvim"] = {
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
 
-  -- Motion plugins
-  ["ggandor/leap.nvim"] = {
-    config = function()
-      require('leap').add_default_mappings()
-    end,
-  },
-
-  ["tpope/vim-repeat"] = {},
-
+	["tpope/vim-repeat"] = {},
 }
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,25 +1,25 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-   return
+	return
 end
 
 local b = null_ls.builtins
 
 local sources = {
 
-   -- clojure stuff
-   b.formatting.cljstyle,
+	-- clojure stuff
+	b.formatting.cljstyle,
 
-   -- Lua
-   b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-   -- Shell
-   b.formatting.shfmt,
-   b.diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+	-- Shell
+	b.formatting.shfmt,
+	b.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
 }
 
-null_ls.setup {
-   debug = true,
-   sources = sources,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+})
